de040b49af969d59204136a272741880de251658
Cassio Almeron <cassioalmeron@gmail.com>
2021-03-14 14:42:21 -0300

    Module 07 - Class 05

--- Module07/src/pages/Dashboard/index.tsx
+++ Module07/src/pages/Dashboard/index.tsx
@@ -1,53 +1,66 @@
-import React from 'react'
+import React, { FormEvent, useState } from 'react'
 import { FiChevronRight } from 'react-icons/fi'
 
 import logoImg from '../../assets/logo.svg'
 import {Title, Form, Repositories} from './styles'
+import api from '../../services/api'
 
-const Dashboard:React.FC = () =>
-    <>
+interface Repository {
+    full_name: string;
+    owner: {
+        login: string;
+        avatar_url: string;
+    },
+    description: string;
+}
+
+const Dashboard:React.FC = () => {
+    const [newRepo, setNewRepo] = useState('');
+    const [repositories, setRepositories] = useState<Repository[]>([]);
+
+    async function handleAddRepository(event: FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+
+        const response = await api.get<Repository>(`repos/${newRepo}`);
+
+        const repository = response.data;
+
+        setRepositories([...repositories, repository])
+        setNewRepo('');
+    }
+
+    return <>
         <img src={logoImg} alt="Github Explorer"/>
         <Title>Explore repositórios no GitHub</Title>
 
-        <Form action="">
-            <input placeholder="Digite o nome do repositório"/>
+        <Form onSubmit={handleAddRepository}>
+            <input 
+                value={newRepo}
+                onChange={e => setNewRepo(e.target.value)}
+                placeholder="Digite o nome do repositório"/>
             <button type="submit">Pesquisar</button>
         </Form>
 
         <Repositories>
-            <a href="teste">
-                <img src="https://avatars.githubusercontent.com/u/50529323?s=460&u=c55a50d54de5aeee129b70abd4ec18c3b364db31&v=4" alt="Cássio Almeron"/>
+            {repositories.map(repository =>
+                <a href="teste" key={repository.full_name}>
+                    <img 
+                        src={repository.owner.avatar_url} 
+                        alt={repository.owner.login}
+                    />
 
-                <div>
-                    <strong> cassioalmeron / BootCamp2020</strong>
-                    <p>The developed about the classes and challanges shown on the BootCamp GoStack, trained by RocketSeat</p>
-                </div>
-                
-                <FiChevronRight size={20} />
-            </a>
-
-            <a href="teste">
-                <img src="https://avatars.githubusercontent.com/u/50529323?s=460&u=c55a50d54de5aeee129b70abd4ec18c3b364db31&v=4" alt="Cássio Almeron"/>
-
-                <div>
-                    <strong> cassioalmeron / BootCamp2020</strong>
-                    <p>The developed about the classes and challanges shown on the BootCamp GoStack, trained by RocketSeat</p>
-                </div>
-                
-                <FiChevronRight size={20} />
-            </a>
-
-            <a href="teste">
-                <img src="https://avatars.githubusercontent.com/u/50529323?s=460&u=c55a50d54de5aeee129b70abd4ec18c3b364db31&v=4" alt="Cássio Almeron"/>
-
-                <div>
-                    <strong> cassioalmeron / BootCamp2020</strong>
-                    <p>The developed about the classes and challanges shown on the BootCamp GoStack, trained by RocketSeat</p>
-                </div>
-                
-                <FiChevronRight size={20} />
-            </a>
+                    <div>
+                        <strong>{repository.full_name}</strong>
+                        <p>{repository.description}</p>
+                    </div>
+                    
+                    <FiChevronRight size={20} />
+                </a>
+            )}
+            
+            
         </Repositories>
     </>
+}
 
 export default Dashboard
