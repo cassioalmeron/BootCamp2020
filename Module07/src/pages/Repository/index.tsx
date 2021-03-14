57e7ef05ee76e319c4076cf16f91a49ca2bdd78c
Cassio Almeron <cassioalmeron@gmail.com>
2021-03-14 15:45:28 -0300

    Module 07 - Class 09

--- Module07/src/pages/Repository/index.tsx
+++ Module07/src/pages/Repository/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react'
-import {useRouteMatch} from 'react-router-dom'
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi'
+import {useRouteMatch, Link} from 'react-router-dom'
+
+import logoImg from '../../assets/logo.svg'
+import {Header, RepositoryInfo, Issues} from './styles'
 
 interface RepositoryParams {
     repository: string;
@@ -8,7 +12,50 @@ interface RepositoryParams {
 const Repository:React.FC = () => {
     const { params } = useRouteMatch<RepositoryParams>();
     
-    return <h1>Repository: {params.repository}</h1>
+    return <>
+        <Header>
+            <img src={logoImg} alt="Github Explorer"/>
+            <Link to="/dashboard">
+                <FiChevronLeft size={16} />
+                Voltar
+            </Link>
+        </Header>
+
+        <RepositoryInfo>
+            <header>
+                <img src="https://avatars.githubusercontent.com/u/50529323?s=460&u=c55a50d54de5aeee129b70abd4ec18c3b364db31&v=4" alt="Cássio Almeron"/>
+                <div>
+                    <strong></strong>
+                    <p></p>
+                </div>
+            </header>
+            <ul>
+                <li>
+                    <strong>1111</strong>
+                    <span>Stars</span>
+                </li>
+                <li>
+                    <strong>1111</strong>
+                    <span>Forks</span>
+                </li>
+                <li>
+                    <strong>1111</strong>
+                    <span>Issues</span>
+                </li>
+            </ul>
+        </RepositoryInfo>
+
+        <Issues>
+            <Link to="">
+                <div>
+                    <strong>sss</strong>
+                    <p>sss</p>
+                </div>
+
+                <FiChevronRight size={20} />
+            </Link>
+        </Issues>
+    </>
 }
 
 export default Repository
