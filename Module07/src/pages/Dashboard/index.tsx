2c99618133e546749c97736f785b61611b3198e9
Cassio Almeron <cassioalmeron@gmail.com>
2021-03-14 15:09:01 -0300

    Module 07 - Class 07

--- Module07/src/pages/Dashboard/index.tsx
+++ Module07/src/pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import React, { FormEvent, useState } from 'react'
+import React, { FormEvent, useState, useEffect } from 'react'
 import { FiChevronRight } from 'react-icons/fi'
 
 import logoImg from '../../assets/logo.svg'
@@ -14,10 +14,25 @@ interface Repository {
     description: string;
 }
 
+const localStorageKey = '@GithubExplorer:repositories'
+
 const Dashboard:React.FC = () => {
     const [newRepo, setNewRepo] = useState('');
     const [inputError, setInputError] = useState('');
-    const [repositories, setRepositories] = useState<Repository[]>([]);
+    const [repositories, setRepositories] = useState<Repository[]>(() => {
+        const storagedRepositories = localStorage.getItem(localStorageKey);
+        if (storagedRepositories)
+            return JSON.parse(storagedRepositories);
+        return []
+    });
+
+    useEffect(()=>{
+        
+    }, [])
+
+    useEffect(()=>{
+        localStorage.setItem(localStorageKey, JSON.stringify(repositories))
+    }, [repositories])
 
     async function handleAddRepository(event: FormEvent<HTMLFormElement>) {
         event.preventDefault();
