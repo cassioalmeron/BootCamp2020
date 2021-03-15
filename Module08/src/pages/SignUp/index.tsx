16fb1e256ad0075d87b3ffe7be1e7d1319ef6960
Cassio Almeron <cassioalmeron@gmail.com>
2021-03-15 13:25:18 -0300

    Module 08 - Class 08

--- Module08/src/pages/SignUp/index.tsx
+++ Module08/src/pages/SignUp/index.tsx
@@ -1,15 +1,26 @@
 import { Form } from '@unform/web';
-import React from 'react';
+import React, { useCallback } from 'react';
 import { FiArrowLeft, FiLock, FiMail, FiUser } from 'react-icons/fi';
+import * as Yup from 'yup';
 import logoImg from '../../assets/logo.svg';
 import Button from '../../components/Button';
 import Input from '../../components/Input';
 import { Background, Container, Content } from './styles';
 
+const schema = Yup.object().shape({
+  name: Yup.string().required('Nome obrigatório'),
+  email: Yup.string().email('E-mail inválido').required('E-mail obrigatório'),
+  password: Yup.string().min(6, 'No mínimo 6 dígitos'),
+});
+
 const SignUp: React.FC = () => {
-  function handleSubmit(data: any): void {
-    console.log(data);
-  }
+  const handleSubmit = useCallback(async (data: any) => {
+    try {
+      await schema.validate(data, { abortEarly: false });
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
 
   return (
     <Container>
