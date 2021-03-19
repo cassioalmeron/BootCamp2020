9233a241f3e2647e35c472b6236a9d56086ec5b9
Cassio Almeron <cassioalmeron@gmail.com>
2021-03-19 18:26:35 -0300

    Module 09 - Class 10

--- Module09/src/pages/SignIn/index.tsx
+++ Module09/src/pages/SignIn/index.tsx
@@ -1,7 +1,13 @@
 import React, { useCallback, useRef } from 'react';
 import Icon from 'react-native-vector-icons/Feather';
 
-import { Image, KeyboardAvoidingView, Platform, View } from 'react-native';
+import {
+  Image,
+  KeyboardAvoidingView,
+  Platform,
+  View,
+  TextInput,
+} from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 import { useNavigation } from '@react-navigation/native';
 import { Form } from '@unform/mobile';
@@ -27,6 +33,8 @@ const SignIn: React.FC = () => {
     console.log(data);
   }, []);
 
+  const passwordInputRef = useRef<TextInput>(null);
+
   const formRef = useRef<FormHandles>(null);
 
   return (
@@ -48,8 +56,27 @@ const SignIn: React.FC = () => {
             </View>
 
             <Form onSubmit={handleSignIn} ref={formRef}>
-              <Input name="email" icon="mail" placeholder="E-mail" />
-              <Input name="password" icon="lock" placeholder="Senha" />
+              <Input
+                autoCorrect={false}
+                autoCapitalize="none"
+                keyboardType="email-address"
+                name="email"
+                icon="mail"
+                placeholder="E-mail"
+                returnKeyType="next"
+                onSubmitEditing={() => passwordInputRef.current?.focus()}
+              />
+
+              <Input
+                name="password"
+                icon="lock"
+                placeholder="Senha"
+                secureTextEntry
+                returnKeyType="send"
+                onSubmitEditing={() => formRef.current?.submitForm()}
+                ref={passwordInputRef}
+              />
+
               <Button onPress={() => formRef.current?.submitForm()}>
                 Entrar
               </Button>
--- Module09/src/pages/SignUp/index.tsx
+++ Module09/src/pages/SignUp/index.tsx
@@ -17,6 +17,9 @@ const SignUp: React.FC = () => {
 
   const formRef = useRef<FormHandles>(null);
 
+  const emailInputRef = useRef<TextInput>(null);
+  const passwordInputRef = useRef<TextInput>(null);
+
   return (
     <>
       <KeyboardAvoidingView
@@ -36,9 +39,37 @@ const SignUp: React.FC = () => {
             </View>
 
             <Form onSubmit={data => console.log(data)} ref={formRef}>
-              <Input name="name" icon="user" placeholder="Nome" />
-              <Input name="email" icon="mail" placeholder="E-mail" />
-              <Input name="password" icon="lock" placeholder="Senha" />
+              <Input
+                autoCapitalize="words"
+                name="name"
+                icon="user"
+                placeholder="Nome"
+                returnKeyType="next"
+                onSubmitEditing={() => emailInputRef.current?.focus()}
+              />
+
+              <Input
+                ref={emailInputRef}
+                keyboardType="email-address"
+                autoCorrect={false}
+                autoCapitalize="none"
+                name="email"
+                icon="mail"
+                placeholder="E-mail"
+                returnKeyType="next"
+                onSubmitEditing={() => passwordInputRef.current?.focus()}
+              />
+
+              <Input
+                ref={passwordInputRef}
+                secureTextEntry
+                textContentType="newPassword"
+                name="password"
+                icon="lock"
+                placeholder="Senha"
+                returnKeyType="send"
+                onSubmitEditing={() => formRef.current?.submitForm()}
+              />
               <Button onPress={() => formRef.current?.submitForm()}>
                 Entrar
               </Button>
