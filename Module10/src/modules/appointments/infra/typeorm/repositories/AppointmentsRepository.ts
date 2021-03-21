28551fb1e75553a987e6e3b9ddfd5e2290736585
Cassio Almeron <cassioalmeron@gmail.com>
2021-03-21 13:31:24 -0300

    Module 10 - Class 07

--- Module10/src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ Module10/src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -1,14 +1,25 @@
+import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
-import { EntityRepository, Repository } from 'typeorm';
+import { EntityRepository, getRepository, Repository } from 'typeorm';
 import Appointment from '../entities/Appointment';
 
 @EntityRepository(Appointment)
-class AppointmentsRepository
-  extends Repository<Appointment>
-  implements IAppointmentsRepository {
+class AppointmentsRepository implements IAppointmentsRepository {
+  private ormRepository: Repository<Appointment>;
+
+  constructor() {
+    this.ormRepository = getRepository(Appointment);
+  }
+
   public async findByDate(date: Date): Promise<Appointment | undefined> {
-    const findAppointment = await this.findOne({ date });
+    const findAppointment = await this.ormRepository.findOne({ date });
     return findAppointment;
+  }
+
+  public async create(data: ICreateAppointmentDTO): Promise<Appointment> {
+    const appointment = this.ormRepository.create(data);
+    await this.ormRepository.save(appointment);
+    return appointment;
   }
 }
 
