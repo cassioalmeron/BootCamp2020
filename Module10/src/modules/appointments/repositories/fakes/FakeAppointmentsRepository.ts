d2bfa9429e292fcc182e390335dab7aacfa87f1e
Cassio Almeron <cassioalmeron@gmail.com>
2021-03-23 10:52:02 -0300

    Module 10 - Class 16

---
+++ Module10/src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -0,0 +1,32 @@
+import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
+import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
+import { uuid } from 'uuidv4';
+import Appointment from '../../infra/typeorm/entities/Appointment';
+
+class FakeAppointmentsRepository implements IAppointmentsRepository {
+  private appointments: Appointment[] = [];
+
+  public async findByDate(date: Date): Promise<Appointment | undefined> {
+    const findAppointment = this.appointments.find(
+      appointment => appointment.date === date,
+    );
+
+    return findAppointment;
+  }
+
+  public async create(data: ICreateAppointmentDTO): Promise<Appointment> {
+    const appointment = new Appointment();
+
+    Object.assign(appointment, {
+      id: uuid(),
+      date: data.date,
+      provider_id: data.provider_id,
+    });
+
+    this.appointments.push(appointment);
+
+    return appointment;
+  }
+}
+
+export default FakeAppointmentsRepository;
