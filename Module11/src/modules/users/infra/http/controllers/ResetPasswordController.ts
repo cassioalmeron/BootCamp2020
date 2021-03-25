2486f3e100acc7c85abc09379b7e89000abd1cdc
Cassio Almeron <cassioalmeron@gmail.com>
2021-03-25 20:05:13 -0300

    Module 11 - Class 06

---
+++ Module11/src/modules/users/infra/http/controllers/ResetPasswordController.ts
@@ -0,0 +1,16 @@
+import { container } from 'tsyringe';
+import { Request, Response } from 'express';
+import ResetPasswordService from '@modules/users/services/ResetPasswordService';
+
+class ResetPasswordController {
+  async create(request: Request, response: Response): Promise<Response> {
+    const { password, token } = request.body;
+
+    const authenticateUser = container.resolve(ResetPasswordService);
+    await authenticateUser.execute({ password, token });
+
+    return response.status(204);
+  }
+}
+
+export default new ResetPasswordController();
