aff1f52f5ce200558805de2e57f6d5d1abdb0abf
Cassio Almeron <cassioalmeron@gmail.com>
2021-03-28 20:59:04 -0300

    Module 11 - Class 20

--- Module11/src/modules/appointments/infra/http/routes/providers.routes.ts
+++ Module11/src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -1,11 +1,21 @@
 import { Router } from 'express';
 import ensudeAuthenticated from '@modules/users/infra/http/middlewares/ensudeAuthenticated';
 import ProvidersController from '../controllers/ProvidersController';
+import ProviderMonthAvailabilityController from '../controllers/ProviderMonthAvailabilityController';
+import ProviderDayAvailabilityController from '../controllers/ProviderDayAvailabilityController';
 
 const providersRouter = Router();
 
 providersRouter.use(ensudeAuthenticated);
 
 providersRouter.get('/', ProvidersController.index);
+providersRouter.get(
+  '/:provider_id/month-availability',
+  ProviderMonthAvailabilityController.index,
+);
+providersRouter.get(
+  '/:provider_id/day-availability',
+  ProviderDayAvailabilityController.index,
+);
 
 export default providersRouter;
