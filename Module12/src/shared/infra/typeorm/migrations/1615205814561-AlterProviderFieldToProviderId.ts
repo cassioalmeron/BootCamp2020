0ff6e8878308092d0166df975ec9715680f32880
Cassio Almeron <cassioalmeron@gmail.com>
2021-05-17 10:21:34 -0300

    Adjust Migration

--- Module12/src/shared/infra/typeorm/migrations/1615205814561-AlterProviderFieldToProviderId.ts
+++ Module12/src/shared/infra/typeorm/migrations/1615205814561-AlterProviderFieldToProviderId.ts
@@ -8,7 +8,7 @@ import {
 export default class AlterProviderFieldToProviderId1615205814561
   implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropColumn('appointments', 'provider');
+    // await queryRunner.dropColumn('appointments', 'provider');
     await queryRunner.addColumn(
       'appointments',
       new TableColumn({
