73f02358192d1d678f7a80a6a489ea2f2bc15665
Cassio Almeron <cassioalmeron@gmail.com>
2021-06-08 15:09:15 -0300

    Module 15 - Class 10

--- Module15/src/__tests__/hooks/auth.spec.tsx
+++ Module15/src/__tests__/hooks/auth.spec.tsx
@@ -1,4 +1,4 @@
-import { renderHook } from '@testing-library/react-hooks';
+import { renderHook, act } from '@testing-library/react-hooks';
 import MockAdapter from 'axios-mock-adapter';
 import {
   AuthProvider,
@@ -45,5 +45,83 @@ describe('Auth hook', () => {
       JSON.stringify(apiResponse.user),
     );
     expect(result.current.user.email).toEqual('cassioalmeron@gmail.com');
+  });
+
+  it('shold restore saved data from storage when auth inits', async () => {
+    jest.spyOn(Storage.prototype, 'getItem').mockImplementation(key => {
+      switch (key) {
+        case localStorageTokenKey:
+          return 'token-123';
+        case localStorageUserKey:
+          return JSON.stringify({
+            id: 'user-123',
+            name: 'Cássio Almeron',
+            email: 'cassioalmeron@gmail.com',
+          });
+        default:
+          return null;
+      }
+    });
+
+    const { result } = renderHook(() => useAuth(), {
+      wrapper: AuthProvider,
+    });
+
+    expect(result.current.user.email).toEqual('cassioalmeron@gmail.com');
+  });
+
+  it('shold be able to sign out', async () => {
+    jest.spyOn(Storage.prototype, 'getItem').mockImplementation(key => {
+      switch (key) {
+        case localStorageTokenKey:
+          return 'token-123';
+        case localStorageUserKey:
+          return JSON.stringify({
+            id: 'user-123',
+            name: 'Cássio Almeron',
+            email: 'cassioalmeron@gmail.com',
+          });
+        default:
+          return null;
+      }
+    });
+
+    const removeItemSpy = jest.spyOn(Storage.prototype, 'removeItem');
+
+    const { result } = renderHook(() => useAuth(), {
+      wrapper: AuthProvider,
+    });
+
+    act(() => {
+      result.current.signOut();
+    });
+
+    expect(removeItemSpy).toHaveBeenCalledTimes(2);
+    expect(result.current.user).toBeUndefined();
+  });
+
+  it('shold be able to update user data', async () => {
+    const setItemSpy = jest.spyOn(Storage.prototype, 'setItem');
+
+    const { result } = renderHook(() => useAuth(), {
+      wrapper: AuthProvider,
+    });
+
+    const user = {
+      id: 'user-123',
+      name: 'Cássio Almeron',
+      email: 'cassioalmeron@gmail.com',
+      avatar_url: 'image-test.jpg',
+    };
+
+    act(() => {
+      result.current.updateUser(user);
+    });
+
+    expect(setItemSpy).toHaveBeenCalledWith(
+      localStorageUserKey,
+      JSON.stringify(user),
+    );
+    expect(result.current.user).toEqual(user);
   });
 });
